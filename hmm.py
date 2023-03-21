50f1d7eabebdcd12acf99560b843b8f5d350a42c
sinlerr <e0775249@u.nus.edu>
2023-03-21 23:27:03 +0800

    completed!!!

--- hmm.py
+++ hmm.py
@@ -1,5 +1,14 @@
+# GROUP: BUZZIN'
+# Kelly Ng Kaiqing (A0240120H), 
+# Lim Xiang Ling (A0238445Y), 
+# Ng Yi Wei (A0238253E), 
+# Tan Sin Ler (A0240651N)
+
 # Implement the six functions below
-#placing possible tags into a list
+
+##################### HELPER FUNCTIONS #######################
+
+# placing possible tags into a list
 def tags(file):
     tags_list = []
     with open(file) as f:
@@ -7,8 +16,8 @@ def tags(file):
             tags_list.append(tag.strip())
     return tags_list
 
-#tokens and their counts and returns a dictionary as follows:
-#{token = x1: count of x1, token = x2: count of x2, ...}
+# tokens and their counts and returns a dictionary as follows:
+# {token = x1: count of x1, token = x2: count of x2, ...}
 def count_words(in_train_filename):
     freqs = {}
     with open(in_train_filename) as f:
@@ -23,8 +32,8 @@ def count_words(in_train_filename):
                     freqs[token] = 1
     return freqs
 
-#tags and their counts and returns a dictionary as follows:
-#{tag = y1: count of y1, tag = y2: count of y2, ...}
+# tags and their counts and returns a dictionary as follows:
+# {tag = y1: count of y1, tag = y2: count of y2, ...}
 def count_tags(in_train_filename):
     freqs = {}
     with open(in_train_filename) as f:
@@ -39,8 +48,8 @@ def count_tags(in_train_filename):
                     freqs[tag] = 1
     return freqs
 
-#returns a dictionary of dictionaries where count the number of token w associated with j 
-#e.g. {tag = y1:{token = x1: count, token = x2: count}, tag = y2:{token = x1: count, token = x2: count}}
+# returns a dictionary of dictionaries where count the number of token w associated with j 
+# e.g. {tag = y1:{token = x1: count, token = x2: count}, tag = y2:{token = x1: count, token = x2: count}}
 def count_tokens_tags(in_train_filename):
     freqs = {}
     with open(in_train_filename) as f:
@@ -61,10 +70,9 @@ def count_tokens_tags(in_train_filename):
                     freqs[tag] = {}
                     tag_tokens = freqs[tag]
                     tag_tokens[token] = 1
-        
     return freqs
 
-#counting the number of unique words
+# counting the number of unique words
 def num_words(in_train_filename):
     freqs = {}
     with open(in_train_filename) as f:
@@ -78,10 +86,10 @@ def num_words(in_train_filename):
                     freqs[token] = 1
     return sum(freqs.values())
 
-#calculates naive output probabilities
+# calculates naive output probabilities
 def calc_output_prob(in_train_filename):
     output_probabilities = {}
-    sigma = 0.1
+    DELTA = 0.1
     words = num_words(in_train_filename)
     tags_dict = count_tags(in_train_filename)
     tags_tokens_dict = count_tokens_tags(in_train_filename)
@@ -90,15 +98,15 @@ def calc_output_prob(in_train_filename):
     output_probabilities["unseen_token_null"] = {}
     for tag in tags_list:
         tag_count = tags_dict[tag]
-        num = sigma
-        den = tag_count + sigma * (words + 1)
+        num = DELTA
+        den = tag_count + DELTA * (words + 1)
         output_probabilities["unseen_token_null"][tag] = num/den
     
     for tag, tags_count in tags_dict.items():
         tags_tokens = tags_tokens_dict[tag]
         for token, tokens_count in tags_tokens.items():
-            numerator = tokens_count + sigma
-            denominator = tags_count + sigma * (words + 1)
+            numerator = tokens_count + DELTA
+            denominator = tags_count + DELTA * (words + 1)
 
             if token in output_probabilities:
                 token_prob_tag = output_probabilities[token]
@@ -109,15 +117,8 @@ def calc_output_prob(in_train_filename):
                 token_prob_tag[tag] = numerator/denominator
     return output_probabilities
 
-#Question 2a
-output_probabilities = calc_output_prob("twitter_train.txt")
-with open('naive_output_probs.txt', 'w') as f:
-    for token, tags_prob in output_probabilities.items():
-        for tag, prob in tags_prob.items():
-            f.write("{} \t {} \t {} \n ".format(tag, token, prob))
-
-#takes in the naive_output_probs txt file and output a dictionary
-#in the followinf form: 
+# takes in the naive_output_probs txt file and output a dictionary
+# in the following form: 
 # {token=x1:{tag=y1:ouput_probability, tag=y2:ouput_probability}, token=x2:{tag=y1:ouput_probability, tag=y2:ouput_probability}}
 def convert_to_dict(in_output_probs_filename):
     output_probabilities = {}
@@ -137,8 +138,15 @@ def convert_to_dict(in_output_probs_filename):
                     token_tags[tag] = prob
     return output_probabilities
 
+################################### QUESTION 2A #####################################
+# For this question, we chose a DELTA value of 0.1.
+output_probabilities = calc_output_prob("twitter_train.txt")
+with open('naive_output_probs.txt', 'w') as f:
+    for token, tags_prob in output_probabilities.items():
+        for tag, prob in tags_prob.items():
+            f.write("{} \t {} \t {} \n ".format(tag, token, prob))
                     
-#Question 2b
+################################### QUESTION 2B #####################################
 def naive_predict(in_output_probs_filename, in_test_filename, out_prediction_filename):
     naive_prediction = []
     naive_output_dict = convert_to_dict(in_output_probs_filename)
@@ -157,14 +165,19 @@ def naive_predict(in_output_probs_filename, in_test_filename, out_prediction_fil
         for prediction in naive_prediction:
             f.write(prediction)
             f.write('\n')
-#Question 2c
-#The accuracy of my prediction is 65.3% (3 s.f.)
 
-#Question 3a
-#Since P(y = j|x = w) = [P(x = w|y = j) * P(y = j)]/P(x = w) = [bj(w) * P(y = j)]/P(x = w) 
-# and since P(x = w) is a constant we can simply find the argmax of bj(w) * P(y = j)
+################################### QUESTION 2C #####################################
+# The accuracy of our prediction is 65.3% (3 s.f.), where the number of correctly 
+# predicted tags / number of predictions is 900/1378.
 
-#Question 3b
+
+################################### QUESTION 3A #####################################
+# Using Bayes' Theorem, P(y = j|x = w) = [P(x = w|y = j) * P(y = j)] / P(x = w) 
+#                                      = [bj(w) * P(y = j)] / P(x = w).
+# and since P(x = w) is a constant we can simply find the argmax of bj(w) * P(y = j).
+# We can make use of bj(w) from question 2.
+
+################################### QUESTION 3B #####################################
 def naive_predict2(in_output_probs_filename, in_train_filename, in_test_filename, out_prediction_filename):
     naive_prediction = []
     tag_count_dict = count_tags(in_train_filename)
@@ -192,9 +205,9 @@ def naive_predict2(in_output_probs_filename, in_train_filename, in_test_filename
             f.write(prediction)
             f.write('\n')                         
 
-#Question 3c
-#The accuracy of my prediction is 69.3% (3 s.f.)
-
+################################### QUESTION 3C #####################################
+# The accuracy of our prediction is 69.3% (3 s.f.), where the number of correctly 
+# predicted tags / number of predictions is 955/1378.
 
 
 
@@ -251,7 +264,7 @@ def run():
     naive_predict2(naive_output_probs_filename, in_train_filename, in_test_filename, naive_prediction_filename2)
     correct, total, acc = evaluate(naive_prediction_filename2, in_ans_filename)
     print(f'Naive prediction2 accuracy:    {correct}/{total} = {acc}')
-"""
+    '''
     trans_probs_filename =  f'{ddir}/trans_probs.txt'
     output_probs_filename = f'{ddir}/output_probs.txt'
 
@@ -269,7 +282,8 @@ def run():
     viterbi_predict2(in_tags_filename, trans_probs_filename2, output_probs_filename2, in_test_filename,
                      viterbi_predictions_filename2)
     correct, total, acc = evaluate(viterbi_predictions_filename2, in_ans_filename)
-    print(f'Viterbi2 prediction accuracy:  {correct}/{total} = {acc}') """
+    print(f'Viterbi2 prediction accuracy:  {correct}/{total} = {acc}') 
+    '''
     
 
 
