58ee3a67ddf7e2defaa11f68da3a7e45a5721a8d
nyiwei <99710151+nyiwei@users.noreply.github.com>
2023-03-31 01:29:05 +0800

    QS 4A done
    attempted 4a

--- hmm.py
+++ hmm.py
@@ -71,6 +71,32 @@ def count_tokens_tags(in_train_filename):
                     tag_tokens = freqs[tag]
                     tag_tokens[token] = 1
     return freqs
+# count number of transitions, returns a dictionary of dictionary which counts j from i
+# e.g. {yt-1 = i1:{yt = j1: count, yt = j2:count}, yt-1 = i2:{yt = j1: count, yt = j2:count}}
+def count_transition_tags(in_train_filename):
+    freqs = {}
+    tags_list = []
+    with open(in_train_filename) as f:
+        for line in f:
+            l = line.strip()
+            if l:
+                temp_list = l.split()
+                tag = temp_list[1]
+                tags_list.append(tag)
+    for num in range(1, len(tags_list)):
+        i = tags_list[num - 1]
+        j = tags_list[num]
+        if i in freqs:
+            inital_to = freqs[i]
+            if j in inital_to:
+                inital_to[j] += 1
+            else:
+                inital_to[j] = 1
+        else:
+            freqs[i] = {}
+            inital_to = freqs[i]
+            inital_to[j] = 1
+    return freqs
 
 # counting the number of unique words
 def num_words(in_train_filename):
@@ -117,10 +143,51 @@ def calc_output_prob(in_train_filename):
                 token_prob_tag[tag] = numerator/denominator
     return output_probabilities
 
+def calc_transition_prob(in_train_filename):
+    trans_probabilities = {}
+    DELTA = 0.1
+    words = num_words(in_train_filename)
+    tags_dict = count_tags(in_train_filename)
+    transition_dict = count_transition_tags(in_train_filename)
+    tags_list = tags("twitter_tags.txt")
+
+    """ trans_prob["unseen_trans_null"] = {}
+    for i in range(0, len(tags_list)):
+        initial = tags_list[i]
+        initial_count = tags_dict[initial]
+        for j in range(0, len(tags_list)):
+            to = tags_list[j]
+            numerator = DELTA
+            denomenator = initial_count + DELTA * (words + 1)
+            trans_prob """
+    # e.g. {yt-1 = i1:{yt = j1: trans, yt = j2:trans}, yt-1 = i2:{yt = j1: trans, yt = j2:trans}}
+    for initial, finals in transition_dict.items():
+        for final, count in finals.items():
+            numerator = count
+            denominator = tags_dict[initial]
+            
+            if initial in trans_probabilities:
+                initial_to = trans_probabilities[initial]
+                initial_to[final] = numerator / denominator
+            
+            else:
+                trans_probabilities[initial] = {}
+                initial_to = trans_probabilities[initial]
+                initial_to[final] = numerator / denominator
+    return trans_probabilities
+
+"""     for tag in tags_list:
+        tag_count = tags_dict[tag]
+        num = DELTA
+        den = tag_count + DELTA * (words + 1)
+        output_probabilities["unseen_token_null"][tag] = num/den """
+
+
+
 # takes in the naive_output_probs txt file and output a dictionary
 # in the following form: 
 # {token=x1:{tag=y1:ouput_probability, tag=y2:ouput_probability}, token=x2:{tag=y1:ouput_probability, tag=y2:ouput_probability}}
-def convert_to_dict(in_output_probs_filename):
+def output_convert_to_dict(in_output_probs_filename):
     output_probabilities = {}
     with open(in_output_probs_filename) as probs_file:
         for line in probs_file:
@@ -138,6 +205,24 @@ def convert_to_dict(in_output_probs_filename):
                     token_tags[tag] = prob
     return output_probabilities
 
+def trans_convert_to_dict(in_trans_probs_filename):
+    trans_probabilities = {}
+    with open(in_trans_probs_filename) as probs_file:
+        for line in probs_file:
+            l = line.strip().split()
+            if l:
+                initial = l[0]
+                final = l[1]
+                prob = float(l[2])
+                if initial in trans_probabilities:
+                    finals_probs = trans_probabilities[initial]
+                    finals_probs[final] = prob
+                else:
+                    trans_probabilities[initial] = {}
+                    finals_probs = trans_probabilities[initial]
+                    finals_probs[final] = prob
+    return trans_probabilities
+
 ################################### QUESTION 2A #####################################
 # For this question, we chose a DELTA value of 0.1.
 output_probabilities = calc_output_prob("twitter_train.txt")
@@ -149,18 +234,18 @@ with open('naive_output_probs.txt', 'w') as f:
 ################################### QUESTION 2B #####################################
 def naive_predict(in_output_probs_filename, in_test_filename, out_prediction_filename):
     naive_prediction = []
-    naive_output_dict = convert_to_dict(in_output_probs_filename)
+    naive_output_dict = output_convert_to_dict(in_output_probs_filename)
     with open(in_test_filename) as test_file:
         for word in test_file:
             token = word.strip()
             if (token):
                 if token in naive_output_dict:
                     token_tags = naive_output_dict[token]
-                    naive_prediction.append(max(token_tags, key=token_tags.get))       
+                    naive_prediction.append(max(token_tags, key=token_tags.get))
                 else:
                     unseen_token = naive_output_dict["unseen_token_null"]
                     naive_prediction.append(max(unseen_token, key=unseen_token.get))
-
+                   
     with open(out_prediction_filename, "w") as f:
         for prediction in naive_prediction:
             f.write(prediction)
@@ -181,7 +266,7 @@ def naive_predict(in_output_probs_filename, in_test_filename, out_prediction_fil
 def naive_predict2(in_output_probs_filename, in_train_filename, in_test_filename, out_prediction_filename):
     naive_prediction = []
     tag_count_dict = count_tags(in_train_filename)
-    naive_output_dict = convert_to_dict(in_output_probs_filename)
+    naive_output_dict = output_convert_to_dict(in_output_probs_filename)
     total_words = sum(tag_count_dict.values())
 
     for token in naive_output_dict:
@@ -209,7 +294,12 @@ def naive_predict2(in_output_probs_filename, in_train_filename, in_test_filename
 # The accuracy of our prediction is 69.3% (3 s.f.), where the number of correctly 
 # predicted tags / number of predictions is 955/1378.
 
-
+################################### QUESTION 4A #####################################
+trans_probabilities = calc_transition_prob("twitter_train.txt")
+with open('trans_probs.txt', 'w') as f:
+    for inital, finals in trans_probabilities.items():
+        for final, prob in finals.items():
+            f.write("{} \t {} \t {} \n ".format(inital, final, prob))
 
 def viterbi_predict(in_tags_filename, in_trans_probs_filename, in_output_probs_filename, in_test_filename,
                     out_predictions_filename):
