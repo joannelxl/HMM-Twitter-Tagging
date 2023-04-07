ba51c1ac4fc7a5f13ab83f1cd115f9cfa4a3bce0
kellyngkq <100149281+kellyngkq@users.noreply.github.com>
2023-04-07 20:49:01 +0800

    q4b

--- hmm.py
+++ hmm.py
@@ -90,9 +90,9 @@ def num_words(in_train_filename):
     return sum(freqs.values())
 
 # calculates naive output probabilities
-def calc_output_prob(in_train_filename):
+def calc_naive_output_prob(in_train_filename):
     output_probabilities = {}
-    DELTA = 0.001
+    DELTA = 0.1
     words = num_words(in_train_filename)
     #tag and its count, number of times the tag appeared
     tags_dict = count_tags(in_train_filename)
@@ -123,6 +123,21 @@ def calc_output_prob(in_train_filename):
                 token_prob_tag[tag] = numerator/denominator
     return output_probabilities
 
+def calc_output_prob(in_train_filename):
+    naive_ouput_probs = calc_naive_output_prob(in_train_filename)
+    #print(naive_ouput_probs)
+    all_tags = tags("twitter_tags.txt")
+    for word in naive_ouput_probs:
+        for tag in all_tags:
+            #check that word does not have the associated tag
+            if (tag not in naive_ouput_probs[word]):
+                prob = naive_ouput_probs["unseen_token_null"][tag]
+                naive_ouput_probs[word][tag] = prob
+    return naive_ouput_probs
+
+#print(calc_output_prob("twitter_train.txt"))
+
+
 # takes in the naive_output_probs txt file and output a dictionary
 # in the following form: 
 # {token=x1:{tag=y1:ouput_probability, tag=y2:ouput_probability}, token=x2:{tag=y1:ouput_probability, tag=y2:ouput_probability}}
@@ -146,7 +161,7 @@ def convert_to_dict(in_output_probs_filename):
 
 ################################### QUESTION 2A #####################################
 # For this question, we chose a DELTA value of 0.1.
-output_probabilities = calc_output_prob("twitter_train.txt")
+output_probabilities = calc_naive_output_prob("twitter_train.txt")
 with open('naive_output_probs.txt', 'w', encoding="utf-8") as f:
     for token, tags_prob in output_probabilities.items():
         for tag, prob in tags_prob.items():
@@ -220,8 +235,8 @@ def naive_predict2(in_output_probs_filename, in_train_filename, in_test_filename
 ############################# HELPER FUNCTIONS FOR Q4&5 ###############################
 
 #q4a output probabilities
-output_probabilities = calc_output_prob("twitter_train.txt")
-with open('output_probs.txt', 'w', encoding="utf-8") as f:
+output_probabilities = calc_naive_output_prob("twitter_train.txt")
+with open('output_probs.txt', 'w', encoding = "utf-8") as f:
     for token, tags_prob in output_probabilities.items():
         for tag, prob in tags_prob.items():
             f.write("{} \t {} \t {} \n ".format(tag, token, prob))
@@ -247,9 +262,9 @@ def transition_tags(in_train_filename):
                 tags.append('START')
                 startCount += 1
 
-    print(tags)
+    #print(tags)
          
-    #print(startCount) Ans:1101    
+    #print(startCount) #Ans:1101    
     #dont consider the last one since its a false 'START'    
     for x in range(len(tags)-1):
         i = tags[x-1]
@@ -263,7 +278,7 @@ def transition_tags(in_train_filename):
     freq.pop(("END","START"))
     return freq
 
-print(transition_tags("twitter_train.txt"))
+#print(transition_tags("twitter_train.txt"))
 
 #freq = {tag i:count}
 def count_tag_i(in_train_filename):
@@ -272,6 +287,9 @@ def count_tag_i(in_train_filename):
     return freq
 #print(count_tag_i("twitter_train.txt"))
 
+#need to normalise 
+
+
 def calc_transition_prob(in_train_filename, out_prob_filename):
     #transition_probs = { transition=(i, j} : prob = num }
     transition_probs = {}
@@ -279,7 +297,9 @@ def calc_transition_prob(in_train_filename, out_prob_filename):
     transition_dict = transition_tags(in_train_filename)
     tag_count = count_tag_i(in_train_filename)
     all_tags = tags("twitter_tags.txt")
-    DELTA = 0.001
+    all_tags.append('START')
+    all_tags.append('END')
+    DELTA = 0.1
        
     for transition, count in transition_dict.items():
         numerator = count
@@ -307,12 +327,28 @@ def calc_transition_prob(in_train_filename, out_prob_filename):
                 den = tag_count[transition[0]] + DELTA * (word_count + 1)
                 prob = num/den
                 transition_probs[transition] = prob
-    return transition_probs
-            
-#print(calc_transition_prob("twitter_train.txt", "trans_probs.txt"))
+    #return transition_probs
 
-def check_sum(trans_probs):
-    print(trans_probs)
+
+    #normalise
+
+    sum_dict = count_transition_sum(transition_probs)
+    
+    for transition_tuple, prob in transition_probs.items():
+        start_tag = transition_tuple[0]
+        for i in sum_dict:
+            if (start_tag == i):
+                prob_before_normalising = transition_probs[transition_tuple]
+                prob_after_normalising = prob_before_normalising / sum_dict[i]
+                transition_probs[transition_tuple] = prob_after_normalising
+    return transition_probs
+    
+                
+            
+        
+
+def count_transition_sum(trans_probs):
+    #print(trans_probs)
     temp = {}
     for transition, prob in trans_probs.items():
         if transition[0] in temp:
@@ -322,50 +358,106 @@ def check_sum(trans_probs):
             temp[transition[0]] = prob
     return temp
 
-#print(check_sum(calc_transition_prob("twitter_train.txt", "trans_probs.txt")))
+#print(count_transition_sum(calc_transition_prob("twitter_train.txt", "trans_probs.txt")))
             
-        
+
 
 #q4b initilise viterbi
 
-def calc_viterbi(tags, tweet, trans_probs, output_probs):
-    # for state 1 to N:
-    #  viterbi[1,s] <- initialProb * bj
-    #  backpointer <- 0
+def calc_viterbi(tags, transition_probs, output_probs, tweet):
+    pi_and_bp = {}
+    #word num is the step count
+    word_num = 1
 
-    #compute initial probabilities for each tag
-    initial_probs = {}
-    for transition, prob in trans_probs.items():
-        tag_i = transition[0]
-        if tag_i == "START":
-            initial_probs[transition] = prob    
-
-
-    viterbi = []
-    for i in tweet:
-        #i represents a word in the tweet
+    for i in range(len(tweet)):
+        word = tweet[i]
         for tag in tags:
-            #tag is a possible tag
-            if tweet.index(i) == 0:
-                transition = ('START', tag)
-                pi = trans_probs[transition]
-                viterbi.append(pi)
-            else:
-                index_of_prev_tag = tags.index(tag) - 1
-                prev_tag = tags[index_of_prev_tag]
-                transition = (prev_tag, tag)
-                if output_probs.get(i) is not None:
-                    if output_probs[i].get(tag) is not None:
-                        pi = output_probs[i][tag] * trans_probs[transition]
-                        viterbi.append(pi)
+            #check for first word of tweet
+            if (i == 0):
+                if (word in output_probs):
+                    pi = transition_probs['START', tag] * output_probs[word][tag]
+                    if (word_num not in pi_and_bp): 
+                        pi_and_bp[word_num] = {}
+                        pi_and_bp[word_num][tag] = [pi,'START']
+                    else:
+                        pi_and_bp[word_num][tag] = [pi,'START']
+
                 else:
-                    pi = output_probs['unseen_token_null'][tag] * trans_probs[transition]
-                    viterbi.append(pi)
+                    #if word is not in output_probs
+                    pi = transition_probs['START', tag] * output_probs['unseen_token_null'][tag]
+                    if (word_num not in pi_and_bp): 
+                        pi_and_bp[word_num] = {}
+                        pi_and_bp[word_num][tag] = [pi,'START']
+                    else:
+                        pi_and_bp[word_num][tag] = [pi,'START']
+
+
+            else:
+                #prev pi * transition * output 
+                #store all pies, find max, then push into dict
+                #so pi_and_bp will alw contain the max pi and backpointer
+                #{pi:BP}
+                tempDict = {}
+                prev = word_num - 1
+
+                for prevTag, pi in pi_and_bp[prev].items():
+                    transition = (prevTag, tag)
+                    if (word in output_probs):
+                        new_pi = pi[0] * transition_probs[transition] * output_probs[word][tag]
+                        tempDict[new_pi] = prevTag
+                    else:
+                        new_pi = pi[0] * transition_probs[transition] * output_probs['unseen_token_null'][tag]
+                        tempDict[new_pi] = prevTag
+
+                sorted_dict = sorted(tempDict.items())
+                #print(sorted_dict)
                 
-    return viterbi
-            
+                max_pi = sorted_dict[-1][0]
+                max_bp = sorted_dict[-1][1]
+                if (word_num not in pi_and_bp):
+                    pi_and_bp[word_num] = {}
+                    pi_and_bp[word_num][tag] = [max_pi, max_bp]
+                else:
+                    pi_and_bp[word_num][tag] = [max_pi, max_bp]
         
+        word_num += 1
+        
+
+    #last number, all multiply by transition from tag to stop
+    #print(pi_and_bp)
+    for i in pi_and_bp:
+        if (i == len(tweet)):
+            for tag in pi_and_bp[i]:
+                for max_pi_and_bp in pi_and_bp[i][tag]:
+                    #update with new value
+                    transition = (tag, 'END')
+                    curr_prob = pi_and_bp[i][tag][0]
+                    pi_and_bp[i][tag][0] = transition_probs[transition] * curr_prob      
+
+    sorted_pi_and_bp = dict(sorted(pi_and_bp.items(),reverse=True))
+
+    #find max, store BP
+    final_max = dict(sorted(sorted_pi_and_bp[len(tweet)].items(), key = lambda item: item[1], reverse = True))
     
+    finalBP = next(iter((final_max.items())))[1][1]
+    allBackPointers = {len(tweet):finalBP}
+
+    for i in sorted_pi_and_bp:
+        for tag, pi_bp in sorted_pi_and_bp[i].items():
+            if (tag == allBackPointers[i]):
+                newBP = pi_bp[1]
+                backPointer = newBP
+                #append curr tag to allBackPointers
+                allBackPointers[i-1] = backPointer
+
+    sequence = []
+    allBackPointers = dict(sorted(allBackPointers.items()))
+
+    for i in allBackPointers:
+        if (i != 0):
+            sequence.append(allBackPointers[i])
+
+    return(sequence)
 
 
 ################################# QUESTION 4A ################################
@@ -374,6 +466,11 @@ with open('trans_probs.txt', 'w', encoding="utf-8") as f:
     for i_to_j, trans_prob in transition_probabilities.items():
         f.write("{} \t {} \t {} \n ".format(i_to_j[0], i_to_j[1], trans_prob))
 
+output_probabilities = calc_output_prob("twitter_train.txt")
+with open('output_probs.txt', 'w', encoding = "utf-8") as f:
+    for token, tags_prob in output_probabilities.items():
+        for tag, prob in tags_prob.items():
+            f.write("{} \t {} \t {} \n ".format(tag, token, prob))
 
 def viterbi_predict(in_tags_filename, in_trans_probs_filename, in_output_probs_filename, in_test_filename,
                     out_predictions_filename):
@@ -384,12 +481,6 @@ def viterbi_predict(in_tags_filename, in_trans_probs_filename, in_output_probs_f
     with open(in_tags_filename, encoding = "utf-8") as f:
         for line in f:
             tags.append(line.strip())
-
-    #read the tokens
-    tokens = []
-    with open(in_test_filename, encoding = "utf-8") as f:
-        for line in f:
-            tokens.append(line.strip())
 
     #read the tweets
     all_tweets = []
@@ -405,13 +496,25 @@ def viterbi_predict(in_tags_filename, in_trans_probs_filename, in_output_probs_f
                 all_tweets.append(tweet)
                 tweet = []
 
-    #iterate through all tweets, each time, calculate viterbi for it
-    for i in all_tweets:
-        return calc_viterbi(tags, i, transition_probs, output_probs)
-            
-#print(viterbi_predict("twitter_tags.txt", "trans_probs.txt", "output_probs.txt", "twitter_dev_no_tag.txt",
-                    #"viterbi_predictions.txt"))
+    viterbi_dict = {}
     
+    #iterate through all tweets, each time, calculate viterbi for it
+
+    for i in all_tweets:
+        temp_seq = calc_viterbi(tags, transition_probs, output_probs, i)
+        viterbi_dict[all_tweets.index(i)+1] = temp_seq
+    ##return viterbi_dict
+
+
+    with open(out_predictions_filename, "w") as f:
+        for tweet in viterbi_dict:
+            for prediction in viterbi_dict[tweet]:
+                f.write(prediction)
+                f.write('\n')
+
+                    
+print(viterbi_predict("twitter_tags.txt", 'trans_probs.txt', 'output_probs.txt', 'twitter_dev_ans.txt',
+                      'viterbi_predictions.txt')) 
 
 
       
@@ -448,34 +551,36 @@ def run():
 
     ddir = ''  # your working dir
 
-    in_train_filename = f'{ddir}twitter_train.txt'
+    in_train_filename = 'twitter_train.txt'
 
-    naive_output_probs_filename = f'{ddir}naive_output_probs.txt'
+    naive_output_probs_filename = 'naive_output_probs.txt'
 
-    in_test_filename = f'{ddir}twitter_dev_no_tag.txt'
-    in_ans_filename  = f'{ddir}twitter_dev_ans.txt'
-    naive_prediction_filename = f'{ddir}naive_predictions.txt'
+    in_test_filename = 'twitter_dev_no_tag.txt'
+    in_ans_filename  = 'twitter_dev_ans.txt'
+    naive_prediction_filename = 'naive_predictions.txt'
     naive_predict(naive_output_probs_filename, in_test_filename, naive_prediction_filename)
     correct, total, acc = evaluate(naive_prediction_filename, in_ans_filename)
     print(f'Naive prediction accuracy:     {correct}/{total} = {acc}')
 
 
-    naive_prediction_filename2 = f'{ddir}naive_predictions2.txt'
+    naive_prediction_filename2 = 'naive_predictions2.txt'
     naive_predict2(naive_output_probs_filename, in_train_filename, in_test_filename, naive_prediction_filename2)
     correct, total, acc = evaluate(naive_prediction_filename2, in_ans_filename)
     print(f'Naive prediction2 accuracy:    {correct}/{total} = {acc}')
-    '''
-    trans_probs_filename =  f'{ddir}/trans_probs.txt'
-    output_probs_filename = f'{ddir}/output_probs.txt'
+    
+    trans_probs_filename =  'trans_probs.txt'
+    output_probs_filename = 'output_probs.txt'
 
-    in_tags_filename = f'{ddir}/twitter_tags.txt'
-    viterbi_predictions_filename = f'{ddir}/viterbi_predictions.txt'
+    
+    in_tags_filename = 'twitter_tags.txt'
+    viterbi_predictions_filename = 'viterbi_predictions.txt'
     viterbi_predict(in_tags_filename, trans_probs_filename, output_probs_filename, in_test_filename,
                     viterbi_predictions_filename)
     correct, total, acc = evaluate(
         viterbi_predictions_filename, in_ans_filename)
     print(f'Viterbi prediction accuracy:   {correct}/{total} = {acc}')
 
+    '''
     trans_probs_filename2 = f'{ddir}/trans_probs2.txt'
     output_probs_filename2 = f'{ddir}/output_probs2.txt'
 
@@ -485,6 +590,7 @@ def run():
     correct, total, acc = evaluate(viterbi_predictions_filename2, in_ans_filename)
     print(f'Viterbi2 prediction accuracy:  {correct}/{total} = {acc}') 
     '''
+    
     
 
 
